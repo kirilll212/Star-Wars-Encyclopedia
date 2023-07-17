9369d5bcb51806714f662dc22a9ae7b71f7a973e
kirilll212 <kushkiril2005@gmail.com>
2023-07-17 17:19:58 +0300

    first step

---
+++ src/components/Encyclopedia.jsx
@@ -0,0 +1,206 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Form, Button, Card, Container, Row, Col, Nav, Modal } from 'react-bootstrap';
+
+const SWAPI_BASE_URL = 'https://swapi.dev/api';
+
+const Encyclopedia = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [activeTab, setActiveTab] = useState('characters');
+  const [showModal, setShowModal] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [characterData, setCharacterData] = useState([]);
+  const [planetData, setPlanetData] = useState([]);
+  const [starshipData, setStarshipData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const characterResponse = await axios.get(`${SWAPI_BASE_URL}/people/`);
+      setCharacterData(characterResponse.data.results);
+
+      const planetResponse = await axios.get(`${SWAPI_BASE_URL}/planets/`);
+      setPlanetData(planetResponse.data.results);
+
+      const starshipResponse = await axios.get(`${SWAPI_BASE_URL}/starships/`);
+      setStarshipData(starshipResponse.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const searchItems = async () => {
+    try {
+      let url = '';
+      switch (activeTab) {
+        case 'characters':
+          url = `${SWAPI_BASE_URL}/people/?search=${searchTerm}`;
+          break;
+        case 'planets':
+          url = `${SWAPI_BASE_URL}/planets/?search=${searchTerm}`;
+          break;
+        case 'starships':
+          url = `${SWAPI_BASE_URL}/starships/?search=${searchTerm}`;
+          break;
+        default:
+          break;
+      }
+
+      if (url) {
+        const response = await axios.get(url);
+        setSearchResults(response.data.results);
+      } else {
+        setSearchResults([]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+    setSearchTerm('');
+    setSearchResults([]);
+  };
+
+  const handleCardClick = (item) => {
+    setSelectedItem(item);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedItem(null);
+  };
+
+  const getTabData = () => {
+    switch (activeTab) {
+      case 'characters':
+        return characterData;
+      case 'planets':
+        return planetData;
+      case 'starships':
+        return starshipData;
+      default:
+        return [];
+    }
+  };
+
+  return (
+    <Container className="my-4">
+      <h1 className="mb-4">Star Wars Encyclopedia</h1>
+      <Nav variant="tabs" activeKey={activeTab} onSelect={handleTabChange} className="mb-3">
+        <Nav.Item>
+          <Nav.Link eventKey="characters">Characters</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="planets">Planets</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="starships">Starships</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Form>
+        <Form.Group controlId="searchForm" className="mb-3">
+          <Form.Control
+            type="text"
+            placeholder={`Search for ${activeTab}`}
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </Form.Group>
+        <Button variant="primary" onClick={searchItems} className="mb-3">
+          Search
+        </Button>
+      </Form>
+      <Row xs={1} md={2} lg={3} xl={4} className="g-4">
+        {searchResults.length > 0
+          ? searchResults.map((item, index) => (
+              <Col key={index}>
+                <Card className="h-100" onClick={() => handleCardClick(item)}>
+                  <Card.Body>
+                    <Card.Title>{item.name}</Card.Title>
+                    {activeTab === 'characters' && (
+                      <>
+                        <Card.Text>Height: {item.height}</Card.Text>
+                        <Card.Text>Mass: {item.mass}</Card.Text>
+                      </>
+                    )}
+                    {activeTab === 'planets' && (
+                      <>
+                        <Card.Text>Population: {item.population}</Card.Text>
+                        <Card.Text>Terrain: {item.terrain}</Card.Text>
+                      </>
+                    )}
+                    {activeTab === 'starships' && (
+                      <>
+                        <Card.Text>Model: {item.model}</Card.Text>
+                        <Card.Text>Manufacturer: {item.manufacturer}</Card.Text>
+                      </>
+                    )}
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          : getTabData().map((item, index) => (
+              <Col key={index}>
+                <Card className="h-100" onClick={() => handleCardClick(item)}>
+                  <Card.Body>
+                    <Card.Title>{item.name}</Card.Title>
+                    {activeTab === 'characters' && (
+                      <>
+                        <Card.Text>Height: {item.height}</Card.Text>
+                        <Card.Text>Mass: {item.mass}</Card.Text>
+                      </>
+                    )}
+                    {activeTab === 'planets' && (
+                      <>
+                        <Card.Text>Population: {item.population}</Card.Text>
+                        <Card.Text>Terrain: {item.terrain}</Card.Text>
+                      </>
+                    )}
+                    {activeTab === 'starships' && (
+                      <>
+                        <Card.Text>Model: {item.model}</Card.Text>
+                        <Card.Text>Manufacturer: {item.manufacturer}</Card.Text>
+                      </>
+                    )}
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+      </Row>
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedItem && (
+            <div>
+              {Object.keys(selectedItem).map((key, index) => (
+                <p key={index}>
+                  <strong>{key}:</strong> {selectedItem[key]}
+                </p>
+              ))}
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Container>
+  );
+};
+
+export default Encyclopedia;
